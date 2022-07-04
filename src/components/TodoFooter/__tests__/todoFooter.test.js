37462eb504b39d85cd39ffb1584f77c178667b04
Sincerely Brittany <sincerelybr@gmail.com>
2022-07-04 13:44:06 -0400

    adds a describe block

--- src/components/TodoFooter/__tests__/todoFooter.test.js
+++ src/components/TodoFooter/__tests__/todoFooter.test.js
@@ -23,51 +23,52 @@ const MockTodoFooter = ({ numberOfIncompleteTasks }) => {
     </BrowserRouter>
   );
 };
+describe("TodoFooter", () => {
+  test("should render the correct amount of incomplete tasks", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={5} />);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 
-test("should render the correct amount of incomplete tasks", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={5} />);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
+  test("should render the correct amount of incomplete task", () => {
+    render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+    const paragraphElement = screen.getByText(/1 task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-test("should render the correct amount of incomplete task", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
+// test("assertion toBeVisible test", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/hello/i);
+//   expect(paragraphElement).toBeVisible;
+// });
 
-test("assertion toBeVisible test", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/hello/i);
-  expect(paragraphElement).toBeVisible;
-});
+// test("assertion toContainHTML", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).toContainHTML("p");
+// });
 
-test("assertion toContainHTML", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).toContainHTML("p");
-});
+// test("assertion Not toContainHTML", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement).not.toContainHTML("h3");
+// });
 
-test("assertion Not toContainHTML", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement).not.toContainHTML("h3");
-});
+// test("assertion toHaveTextContent", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByTestId("hello");
+//   expect(paragraphElement).toHaveTextContent("hello");
+// });
 
-test("assertion toHaveTextContent", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("hello");
-  expect(paragraphElement).toHaveTextContent("hello");
-});
+// test("assertion toBeFalsy", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByTestId("hello");
+//   expect(paragraphElement).not.toBeFalsy();
+// });
 
-test("assertion toBeFalsy", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByTestId("hello");
-  expect(paragraphElement).not.toBeFalsy();
-});
-
-test("assertion toBeFalsy", () => {
-  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
-  const paragraphElement = screen.getByText(/1 task left/i);
-  expect(paragraphElement.textContent).toBe("1 task left");
-});
+// test("assertion toBeFalsy", () => {
+//   render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+//   const paragraphElement = screen.getByText(/1 task left/i);
+//   expect(paragraphElement.textContent).toBe("1 task left");
+// });
